1b8e4b5d81e10084c52b4837173429b44de85ea2
maxvanbel <maxvanbel@gmail.com>
2024-01-31 17:30:05 -0600

    Bug: remove items with 0 qty in localStorage

--- src/app/components/Badges.tsx
+++ src/app/components/Badges.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import { useCartContext } from "@/app/Contexts/CartContext";
+import { exit } from "process";
 
 const Badges = () => {
   const { totalQuantityInCart, setExistingCart } = useCartContext();
@@ -9,12 +10,16 @@ const Badges = () => {
   useEffect(() => {
     const existingCart = JSON.parse(localStorage.getItem("cart") || "{}");
     const cartIds = Object.keys(existingCart);
+    if (cartIds.length === 0) {
+      return;
+    }
     const cart = cartIds.map((id) => {
       return {
         id,
         quantity: existingCart[id],
       };
-    });
+    }).filter(item => item.quantity > 0);
+    localStorage.setItem("cart", JSON.stringify(cart));
     setExistingCart(cart);
   }, []);
 
