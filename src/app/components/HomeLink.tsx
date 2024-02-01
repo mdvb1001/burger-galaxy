07b089902291b1c392ccee743d921d33785bd58d
maxvanbel <maxvanbel@gmail.com>
2024-02-01 02:03:52 -0600

    add sparkle touch

--- src/app/components/HomeLink.tsx
+++ src/app/components/HomeLink.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { SparkleIcon } from "@/app/icons/SparkleIcon";
+import Sparkles from "react-sparkle";
 
 const HomeLink = () => {
   const router = useRouter();
@@ -11,13 +12,24 @@ const HomeLink = () => {
   };
   return (
     <a
-      className="font-bold text-inherit flex flex-row cursor-pointer"
+      className="font-bold text-inherit flex flex-row cursor-pointer relative"
       onClick={handleClick}
       role="link"
       tabIndex={0}
       aria-label="go to home page"
     >
-      <span className="mr-2">Burger Galaxy</span>
+      <span className="mr-2">Burger Galaxy
+      <Sparkles
+        color="teal"
+        fadeOutSpeed={10}
+        newSparkleOnFadeOut={false}
+        flicker
+        flickerSpeed="slow"
+        count={80}
+        minSize={5}
+        maxSize={8}
+        overflowPx={25}
+      /></span>
       <SparkleIcon />
     </a>
   );
